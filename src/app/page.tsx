6362d3d195c16e8e19f44d6fc68e5f1d3cdeb6cf
Herquiloide Hele <herquiloide.hele@wit-software.com>
2024-09-01 22:48:27 +0100

    implement header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.scss";
 import HeroSection from "@/app/sections/hero/HeroSection";
+import ServicesSection from "@/app/sections/services/ServicesSection";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <HeroSection />
+      <ServicesSection />
     </main>
   );
 }
---
+++ src/app/sections/services/ServicesSection.tsx
@@ -0,0 +1,9 @@
+import styles from "./ServicesSection.module.scss";
+
+export default function ServicesSection() {
+  return (
+    <div className={styles.container}>
+      <h1></h1>
+    </div>
+  );
+}
