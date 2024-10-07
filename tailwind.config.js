2504aff742d9ec0d92effc9372978e6b4eff4a1a
Herquiloide Hele <herquiloide.hele@wit-software.com>
2024-10-07 21:11:33 +0100

    implement tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/preline/preline.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("preline/plugin")],
+};
