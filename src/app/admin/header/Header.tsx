3fdfc6d7b4d46d0c4f2ec520ede997d23d74da6a
Herquiloide Hele <herquiloide@gmail.com>
2024-10-12 09:59:53 +0100

    improvments on header

--- src/app/admin/header/Header.tsx
+++ src/app/admin/header/Header.tsx
@@ -1,4 +1,6 @@
 import AppLogo from "@/app/ui/vectors/logo-horizontal.svg";
+import AvatarUser from "@/app/ui/images/avatarFallback.png";
+import Image from "next/image";
 
 export default function Header() {
   return (
@@ -14,7 +16,7 @@ export default function Header() {
             aria-label="Toggle navigation"
             data-hs-overlay="#hs-application-sidebar"
           >
-            <span className="sr-only">Toggle Navigation</span>
+            <span className="sr-only">Abrir menu</span>
             <svg
               className="shrink-0 size-4"
               xmlns="http://www.w3.org/2000/svg"
@@ -76,9 +78,9 @@ export default function Header() {
                 aria-expanded="false"
                 aria-label="Dropdown"
               >
-                <img
+                <Image
                   className="shrink-0 size-[38px] rounded-full"
-                  src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                  src={AvatarUser}
                   alt="Avatar"
                 />
               </button>
@@ -91,10 +93,10 @@ export default function Header() {
               >
                 <div className="py-3 px-5 bg-gray-100 rounded-t-lg dark:bg-neutral-700">
                   <p className="text-sm text-gray-500 dark:text-neutral-500">
-                    Signed in as
+                    Autenticado como
                   </p>
                   <p className="text-sm font-medium text-gray-800 dark:text-neutral-200">
-                    james@site.com
+                    jonnebp@gmail.com
                   </p>
                 </div>
                 <div className="p-1.5 space-y-0.5">
@@ -117,51 +119,7 @@ export default function Header() {
                       <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
                       <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
                     </svg>
-                    Newsletter
-                  </a>
-                  <a
-                    className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                    href="#"
-                  >
-                    <svg
-                      className="shrink-0 size-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
-                      <path d="M3 6h18" />
-                      <path d="M16 10a4 4 0 0 1-8 0" />
-                    </svg>
-                    Purchases
-                  </a>
-                  <a
-                    className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                    href="#"
-                  >
-                    <svg
-                      className="shrink-0 size-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
-                      <path d="M12 12v9" />
-                      <path d="m8 17 4 4 4-4" />
-                    </svg>
-                    Downloads
+                    Notificações
                   </a>
                   <a
                     className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
@@ -184,7 +142,29 @@ export default function Header() {
                       <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                       <path d="M16 3.13a4 4 0 0 1 0 7.75" />
                     </svg>
-                    Team Account
+                    Minha Conta
+                  </a>
+                  <a
+                    className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
+                    href="#"
+                  >
+                    <svg
+                      className="shrink-0 size-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+                      <path d="M12 12v9" />
+                      <path d="m8 17 4 4 4-4" />
+                    </svg>
+                    Sair
                   </a>
                 </div>
               </div>
