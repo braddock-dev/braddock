9c36dcb5c2ad0363642a0ce94a6a4532c83680e2
Herquiloide Hele <herquiloide@gmail.com>
2025-01-01 17:36:55 +0000

    configure sentry

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/nextjs";
+
 export async function register() {
   if (
     process.env.NEXT_RUNTIME === "nodejs" &&
@@ -8,4 +10,14 @@ export async function register() {
     server.listen();
     console.log("Server listening on mock server", [process.env.NODE_ENV]);
   }
+
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("../sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("../sentry.edge.config");
+  }
 }
+
+export const onRequestError = Sentry.captureRequestError;
